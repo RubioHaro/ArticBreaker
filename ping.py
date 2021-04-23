d434b0e4e8fe131c1e79b9004a57bb39af283437
Rodrigo Rubio Haro <chavo0022009@gmail.com>
2021-04-23 03:04:57 -0500

    Ping with Paramiko

--- ping.py
+++ ping.py
@@ -2,53 +2,32 @@
 
 import os
 import threading
+import paramiko
+import getpass
 
-from multiprocessing import Process
+def getPing (ping_command, parsed_ip):
+    response = os.popen(ping_command).read() + "--"
+    if response != "--":
+        answered_ips.append(parsed_ip)
 
-def obtienePing (cPing, listip):
-    resp = os.popen(cPing).read()+"--"
-    if resp != "--":
-        ipValidadas.append(resp)
-        listip.append(resp)
-        print(len(listip))
-        print(resp)
-
-def f(start, stop, composicionIp, listip_n):
+def pingTypeC(start, stop, ip):
     for i in range(start, stop):
-        comandoPing = ""
-        comandoPing= "ping -c 1 "+composicionIp[0]+"."+composicionIp[1]+"."+composicionIp[2]+"."+str(i+1)+" | grep ttl"
-        obtienePing(comandoPing, listip_n)
-        print ("TIPO 255.255.255.X ANALIZANDO IP: "+composicionIp[0]+"."+composicionIp[1]+"."+composicionIp[2]+"."+str(i+1))
+        ping_command= "ping -c 1 "+ ip[0]+"."+ip[1]+"."+ip[2]+"."+str(i+1)+" | grep ttl"
+        parsed_ip =  ip[0]+"."+ip[1]+"."+ip[2]+"."+str(i+1)
+        getPing(ping_command, parsed_ip)
+        print ("ping to:" + parsed_ip)
 
 def hacerPin():
-    tem = dirIP[0]
-    composicionIp = tem.split(".")
-    comandoPing = ""
-    if typOcteto == 8:
-        for i in range(254):
-            for j in range(254):
-                for k in range(254):
-                    comandoPing = ""
-                    comandoPing= "ping -c 1 "+composicionIp[0]+"."+str(i+1)+"."+str(j+1)+"."+str(k+1)+" | grep ttl"
-                    obtienePing(comandoPing)
-                    os.system("clear")
-                    print ("RED DE TIPO 255.X.X.X ANALIZANDO IP: "+composicionIp[0]+"."+str(i+1)+"."+str(j+1)+"."+str(k+1))
-    elif typOcteto == 16:
-        for i in range(254):
-            for j in range(254):
-                comandoPing = ""
-                comandoPing= "ping -c 1 "+composicionIp[0]+"."+composicionIp[1]+"."+str(i+1)+"."+str(j+1)+" | grep ttl"
-                obtienePing(comandoPing)
-                os.system("clear")
-                print ("RED 255.255.X.X ANALIZANDO IP: "+composicionIp[0]+"."+composicionIp[1]+"."+str(i+1)+"."+str(j+1))
-    else:
+    tem = ip_dir[0]
+    ip = tem.split(".")
+    if type != 8 and type != 8:
 
         ## Timing 2.39m 5 processes Original time: 13.13m lineal
-        x1 = threading.Thread(target=f,args=(0,51, composicionIp, listip_1,))#Generar hilo
-        x2 = threading.Thread(target=f,args=(52,101, composicionIp, listip_2,))#Generar hilo
-        x3 = threading.Thread(target=f,args=(102,153, composicionIp, listip_3,))#Generar hilo
-        x4 = threading.Thread(target=f,args=(154,203, composicionIp, listip_4,))#Generar hilo
-        x5 = threading.Thread(target=f,args=(204,254, composicionIp, listip_5,))#Generar hilo
+        x1 = threading.Thread(target=pingTypeC,args=(0,51, ip,)) #Generar hilo
+        x2 = threading.Thread(target=pingTypeC,args=(52,101, ip,)) #Generar hilo
+        x3 = threading.Thread(target=pingTypeC,args=(102,153, ip,)) #Generar hilo
+        x4 = threading.Thread(target=pingTypeC,args=(154,203, ip,)) #Generar hilo
+        x5 = threading.Thread(target=pingTypeC,args=(204,254, ip,)) #Generar hilo
         
         x1.start()
         x2.start()
@@ -61,45 +40,73 @@ def hacerPin():
         x3.join()
         x4.join()
         x5.join()
+    else:
+        print("The program only works for 255.255.255.x address type")
 
-ipValidadas = []
+answered_ips = []
 
-listip_1 = []
-listip_2 = []
-listip_3 = []
-listip_4 = []
-listip_5 = []
-
-comandoIP = "ip addr | grep inet | grep brd"
-obtieneIP = os.popen(comandoIP).read()
-listaIP = obtieneIP.split("\n")
-dirIni = 0
-dirFin = 0
-listaIP.pop(len(listaIP)-1)
-dirIP = []
-typOcteto = 0
-for i in listaIP:
-    dirIni = i.find("inet")+5
-    dirFin = i.find("brd")-4
-    temp = i[dirIni:dirFin]
-    dirIP.append(temp)
-    dirIni = dirFin+1
-    dirFin = dirIni+2
-    typOcteto = int(i[dirIni:dirFin])
+ip_list = os.popen("ip addr | grep inet | grep brd").read().split("\n")
+init_dir = 0
+end_dir = 0
+ip_list.pop(len(ip_list)-1)
+ip_dir = []
+type = 0
+for i in ip_list:
+    init_dir = i.find("inet")+5
+    end_dir = i.find("brd")-4
+    temporal_var = i[init_dir:end_dir]
+    ip_dir.append(temporal_var)
+    init_dir = end_dir + 1
+    end_dir = init_dir + 2
+    type = int(i[init_dir:end_dir])
 hacerPin()
-print("Se encontraron:", len(ipValidadas))
-for j in ipValidadas:
+
+print(len(answered_ips), " ips founded!")
+for j in answered_ips:
+    starParamiko(answered_ips)
     print(j)
-    
-print("IP's Encontradas:")
-for j in listip_1:
-    print(j)
-for j in listip_2:
-    print(j)
-for j in listip_3:
-    print(j)
-for j in listip_4:
-    print(j)
-for j in listip_5:
-    print(j)
-print("El programa ha concludio sus procesos.")
+
+print("The program has exited.")
+
+
+def starParamiko(host):
+    print("ArticBraker: Initializing paramiko ssh connection")
+    ssh_client=paramiko.SSHClient()
+
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    print('hostname:', host)
+    host = input()
+
+    print('Enter your username:')
+    user = input()
+
+    try:
+        p = getpass.getpass()
+    except Exception as error:
+        print('ERROR', error)
+    else:
+        print('requesting access.')
+
+    ##ssh_client.connect(hostname,username,input())
+
+    ssh_client.connect(hostname=host, username=user,password=p)
+    print("connected")
+    ##stdin,stdout,stderr=ssh_client.exec_command("mkdir prueba")
+    ##stdin,stdout,stderr=ssh_client.exec_command("ls ./Desktop")
+    ##print(stdout.readlines())
+    ##print(stderr.readlines())
+    ##print('Enter file route:')
+    ##filee = input()
+
+    ftp_client=ssh_client.open_sftp()
+    ArchivoEnviado = "/home/jazmin/Desktop/backupenviado.py"
+    ftp_client.put('/home/jazmin/ArticBreaker/backup.py',ArchivoEnviado) ## de donde va a donde llega 
+    ftp_client.close()
+
+    try:
+        stdin,stdout,stderr=ssh_client.exec_command("python3 "+ArchivoEnviado)
+        print(stdout.readlines())
+        print(stderr.readlines())
+    except:
+        print("An exception occurred")
+
